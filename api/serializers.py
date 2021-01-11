06e236f4e8080009542d094e8cd63e2ab685d1df
root <root@LAPTOP-H5HPC1OF.localdomain>
2021-01-11 18:48:12 +0545

    Permission based actions made

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from .models import Assignment, Question
-
+from .models import Assignment, Question, Choice
+from users.models import User
 
 class StringSerializer(serializers.StringRelatedField):
     def to_internal_value(self, value):
@@ -26,6 +26,35 @@ class AssignmentSerializer(serializers.ModelSerializer):
     def get_questions(self, obj):
         questions = QuestionSerializer(obj.questions.all(), many=True).data
         return questions
+
+    def create(self, request):
+        data = request.data
+        assignment = Assignment()
+        teacher = User.objects.get(username=data['teacher'])
+        assignment.teacher = teacher
+        assignment.title = data['title']
+        assignment.save()
+
+        order = 1
+        for q in data['questions']:
+            newQ = Question()
+            newQ.question = q['title']
+            newQ.order = order
+            newQ.save()
+        
+            for c in q['choices']:
+                newC = Choice()
+                newC.title = c
+                newC.save()
+                newQ.choices.add(newC)
+            
+            newQ.answer = Choice.objects.get(title=q['answer'])
+            newQ.assignment = assignment
+            newQ.save()
+            order += 1
+            return assignment
+        
+
 
 
 
--- api/views.py
+++ api/views.py
@@ -1,6 +1,11 @@
 from .models import Assignment
 from .serializers import AssignmentSerializer
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+)
 # Create your views here.
 
 
@@ -8,3 +13,13 @@ class AssignmentViewSet(viewsets.ModelViewSet):
     serializer_class = AssignmentSerializer
     queryset = Assignment.objects.all()
     
+    def create(self, request):
+        serializer = AssignmentSerializer(data=request.data)
+        if serializer.is_valid():
+            assignment = serializer.create(request)
+            if assignment:
+                return Response(status=HTTP_201_CREATED)
+            return Response(status=HTTP_400_BAD_REQUEST)
+
+
+
